cbefa396b46c4bba2c08825939bea7fd54f85b60
RoyalFlush1010 <nishant.dumbre@gmail.com>
2024-10-24 16:27:27 +0530

    Created basic header

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Header from './Header'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div>
+        <Header />
+        <img src='https://assets.nflxext.com/ffe/siteui/vlv3/74d734ca-0eab-4cd9-871f-bca01823d872/web/IN-en-20241021-TRIFECTA-perspective_2277eb50-9da3-4fdf-adbe-74db0e9ee2cf_medium.jpg' />
+        
+    </div>
   )
 }
 
